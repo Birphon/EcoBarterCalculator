beddb37d5141ca96d314012d9d18fceb28a67b6c
Birphon <61985658+Birphon@users.noreply.github.com>
2022-06-09 14:59:23 +1200

    changes

---
+++ Python/Profession.py
@@ -0,0 +1,8 @@
+class Professions:
+    def __init__(self) -> None:
+        self.name = ""
+        self.hasSpec = False
+        self.specList = []
+
+    def profession_list(self):
+        profList = []
